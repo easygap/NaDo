08bee5f4b2182607edfc6dde7b1d9e955fa5df4e
easygap <dlwnstndlwld@gmail.com>
2024-01-22 09:44:15 +0900

    Initial commit

---
+++ nado/src/main/java/jongol/q562/Main.java
@@ -0,0 +1,21 @@
+package jongol.q562;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int[] num = new int[10];
+
+        int sum = 0;
+        int avg = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = sc.nextInt();
+        }
+
+        
+    }
+}
