ece5274f5185da59c430894bc147e371530cda91
DongKyu Ryu <fbehdrb97@naver.com>
2024-01-24 17:41:40 +0900

    2024-01-24 17:41 외교부_국가·지역별 재외공관 정보 API 구현

--- nado/src/main/java/org/zerock/nado/API/ApiExplorer.java
+++ nado/src/main/java/org/zerock/nado/API/ApiExplorer.java
@@ -1,5 +1,6 @@
 package org.zerock.nado.API;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -9,6 +10,7 @@ import java.net.URL;
 import java.net.URLEncoder;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.List;
 
 public class ApiExplorer {
     public static void main(String[] args) throws IOException {
@@ -17,8 +19,8 @@ public class ApiExplorer {
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]","UTF-8") + "=" + URLEncoder.encode("가나", "UTF-8")); /*한글 국가명*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]","UTF-8") + "=" + URLEncoder.encode("GH", "UTF-8")); /*ISO 2자리코드*/
+        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]","UTF-8") + "=" + URLEncoder.encode("일본", "UTF-8")); /*한글 국가명*/
+        //urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]","UTF-8") + "=" + URLEncoder.encode("", "UTF-8")); /*ISO 2자리코드*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -40,16 +42,26 @@ public class ApiExplorer {
         System.out.println(sb.toString());
     }
 
-    public static String getEmbassyList(String countryName, String isoCode) throws IOException {
+    public static List<EmbassyInfo> getEmbassyList(String countryName) throws IOException {
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1262000/EmbassyService2/getEmbassyList2"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey", "UTF-8") + "=zpDSuAzWoD6LB7nk6hYHS%2FIK%2B0sPm6XquAtZZlTgXOV0C69VWL7Ln5qLzzFzvqOxvUslQZzR6b12Dd%2FwZ7SofQ%3D%3D"); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("returnType", "UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
         urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]", "UTF-8") + "=" + URLEncoder.encode(countryName, "UTF-8")); /*한글 국가명*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]", "UTF-8") + "=" + URLEncoder.encode(isoCode, "UTF-8")); /*ISO 2자리코드*/
+        String apiResponse = sendGetRequest(urlBuilder.toString());
 
-        return sendGetRequest(urlBuilder.toString());
+        // JSON 응답을 EmbassyInfo 리스트로 변환
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(apiResponse);
+        JsonNode dataNode = rootNode.get("data");
+
+        List<EmbassyInfo> embassyList = objectMapper.convertValue(
+                dataNode,
+                new TypeReference<List<EmbassyInfo>>() {}
+        );
+
+        return embassyList;
     }
 
     private static String sendGetRequest(String apiUrl) throws IOException {
@@ -78,13 +90,21 @@ public class ApiExplorer {
         String embassyName = dataNode.get("embassy_kor_nm").asText();
         String embassyAddress = dataNode.get("emblgbd_addr").asText();
         String embassyPhoneNumber = dataNode.get("tel_no").asText();
+        String embassyUrgencyNumber = dataNode.get("urgency_tel_no").asText();
+        String embassyFreeNumber = dataNode.get("free_tel_no").asText();
+        String embassyLat = dataNode.get("embassy_lat").asText();
+        String embassyLng = dataNode.get("embassy_lng").asText();
 
         // 추출된 정보 출력 또는 필요한 처리 수행
         System.out.println("대사관 이름: " + embassyName);
         System.out.println("대사관 주소: " + embassyAddress);
         System.out.println("대사관 전화번호: " + embassyPhoneNumber);
+        System.out.println("대사관 무료 전화번호: " + embassyFreeNumber);
+        System.out.println("대사관 긴급 전화번호: " + embassyUrgencyNumber);
+        System.out.println("대사관 위도: " + embassyLat);
+        System.out.println("대사관 경도: " + embassyLng);
 
         // 필요에 따라 추출된 정보 반환
-        return embassyName + "/ " + embassyAddress + "/ " + embassyPhoneNumber;
+        return embassyName + "/ " + embassyAddress + "/ " + embassyPhoneNumber + "/ " + embassyUrgencyNumber + "/ " + embassyFreeNumber + "/ " + embassyLat + "/ " + embassyLng;
     }
 }
---
+++ nado/src/main/java/org/zerock/nado/API/EmbassyInfo.java
@@ -0,0 +1,33 @@
+package org.zerock.nado.API;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class EmbassyInfo {
+    @JsonProperty("embassy_kor_nm")
+    private String embassyName;
+
+    @JsonProperty("emblgbd_addr")
+    private String embassyAddress;
+
+    @JsonProperty("tel_no")
+    private String embassyPhoneNumber;
+
+    @JsonProperty("free_tel_no")
+    private String embassyFreeNumber;
+
+    @JsonProperty("urgency_tel_no")
+    private String embassyUrgencyNumber;
+
+    @JsonProperty("embassy_lat")
+    private String embassyLat;
+
+    @JsonProperty("embassy_lng")
+    private String embassyLng;
+
+}
