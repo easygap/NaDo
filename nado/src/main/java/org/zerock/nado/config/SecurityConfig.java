af64a66492a164892daa469cd7ce7807248140db
easygap <dlwnstndlwld@gmail.com>
2024-01-26 11:21:01 +0900

    2024 01 26 11:20 페이지 이동 링크 수정

--- nado/src/main/java/org/zerock/nado/config/SecurityConfig.java
+++ nado/src/main/java/org/zerock/nado/config/SecurityConfig.java
@@ -3,47 +3,74 @@ package org.zerock.nado.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.zerock.nado.jwt.JWTFilter;
+import org.zerock.nado.jwt.JWTUtil;
+import org.zerock.nado.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
+
+        this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
+    }
+
     @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        // csrf disable
+        //csrf disable
         http
                 .csrf((auth) -> auth.disable());
 
-        // form 로그인 방식 disable
+        //From 로그인 방식 disable
         http
                 .formLogin((auth) -> auth.disable());
 
-        // http basic 인증 방식 disable
+        //http basic 인증 방식 disable
         http
                 .httpBasic((auth) -> auth.disable());
 
+
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/auth/login", "/auth/join").permitAll()
-                        .requestMatchers("/auth/admin").hasRole("ADMIN")
+                        .requestMatchers("/**", "/login", "/", "/join").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
 
-        // 세션 설정
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        //세션 설정
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
 
         return http.build();
     }
